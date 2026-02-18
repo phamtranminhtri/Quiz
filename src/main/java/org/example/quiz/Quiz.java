1616fbb337cd82e660758b49f78a31892f2b8725
Pham Tran Minh Tri <tri.phamtranminh@hcmut.edu.vn>
2026-02-18 08:39:36 +0700

    Stage 2: add quizzes

--- src/main/java/org/example/quiz/Quiz.java
+++ src/main/java/org/example/quiz/Quiz.java
@@ -1,16 +1,32 @@
 package org.example.quiz;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class Quiz {
+    private static int count = 0;
+    private int id;
     private String title;
     private String text;
     private List<String> options;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private int answer;
 
-    public Quiz(String title, String text, List<String> options) {
+    public Quiz() {
+        count++;
+        id = count;
+    }
+
+    public Quiz(String title, String text, List<String> options, int answer) {
         this.title = title;
         this.text = text;
         this.options = options;
+        this.answer = answer;
+
+        count++;
+        id = count;
     }
 
     public String getTitle() {
@@ -35,5 +51,29 @@ public class Quiz {
 
     public void setOptions(List<String> options) {
         this.options = options;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Quiz.count = count;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
     }
 }
--- src/main/java/org/example/quiz/QuizController.java
+++ src/main/java/org/example/quiz/QuizController.java
@@ -1,38 +1,76 @@
 package org.example.quiz;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class QuizController {
 
     public record QuizResult(boolean success, String feedback) {}
 
-    @GetMapping("/api/quiz")
-    public ResponseEntity<Quiz> getQuiz() {
-        Quiz quiz = new Quiz(
-                "The Java Logo",
-                "What is depicted on the Java logo?",
-                List.of("Robot", "Tea leaf", "Cup of coffee", "Bug")
-        );
+    private Map<Integer, Quiz> quizMap = new HashMap<>();
+
+//    @GetMapping("/api/quiz")
+//    public ResponseEntity<Quiz> getQuiz() {
+//        Quiz quiz = new Quiz(
+//                "The Java Logo",
+//                "What is depicted on the Java logo?",
+//                List.of("Robot", "Tea leaf", "Cup of coffee", "Bug")
+//        );
+//        return new ResponseEntity<>(quiz, HttpStatus.OK);
+//    }
+//
+//    @PostMapping("/api/quiz")
+//    public ResponseEntity<QuizResult> postQuiz(@RequestParam int answer) {
+//        QuizResult quizResult = switch (answer) {
+//            case 2 -> new QuizResult(true, "Congratulations, you're right!");
+//            default -> new QuizResult(false, "Wrong answer! Please, try again.");
+//        };
+//        return new ResponseEntity<>(quizResult, HttpStatus.OK);
+//    }
+
+    @PostMapping("/api/quizzes")
+    public ResponseEntity<Quiz> postQuizzes(@RequestBody Quiz quiz) {
+        quizMap.put(quiz.getId(), quiz);
         return new ResponseEntity<>(quiz, HttpStatus.OK);
     }
 
-    @PostMapping("/api/quiz")
-    public ResponseEntity<QuizResult> postQuiz(@RequestParam int answer) {
-        QuizResult quizResult = switch (answer) {
-            case 2 -> new QuizResult(true, "Congratulations, you're right!");
-            default -> new QuizResult(false, "Wrong answer! Please, try again.");
-        };
-        return new ResponseEntity<>(quizResult, HttpStatus.OK);
+    @GetMapping("/api/quizzes/{id}")
+    public ResponseEntity<Quiz> getQuizzesId(@PathVariable int id) {
+        if (!quizMap.containsKey(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Quiz quiz = quizMap.get(id);
+        return new ResponseEntity<>(quiz, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/quizzes")
+    public ResponseEntity<List<Quiz>> getQuizMap() {
+        List<Quiz> quizList = new ArrayList<>(quizMap.values());
+        return new ResponseEntity<>(quizList, HttpStatus.OK);
+    }
+
+    @PostMapping("/api/quizzes/{id}/solve")
+    public ResponseEntity<QuizResult> postQuizzesIdSolve(@PathVariable int id, @RequestParam int answer) {
+        if (!quizMap.containsKey(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Quiz quiz = quizMap.get(id);
+        int quizAnswer = quiz.getAnswer();
+        QuizResult result;
+        if (answer == quizAnswer) {
+            result = new QuizResult(true, "Congratulations, you're right!");
+        } else {
+            result = new QuizResult(false, "Wrong answer! Please, try again.");
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 }
